f3b9b68913d61d398bf3cfbdde246e7782714a51
Zach Hawkes <zach@ifit.com>
2021-04-13 13:22:23 -0600

    committing some debug code

--- project.cpp
+++ project.cpp
@@ -165,11 +165,12 @@ int main(int argc, char  **argv){
             // - GM broadcasts when a spoon has been taken
             // - Tells a player when they have lost
             if(rank == 0){
-                // tag = -100;
-                MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &tag, MPI_STATUS_IGNORE); // check if a player wants to grab a spoon.
-                if(tag == 1){ // someone wants a spoon;
-                        cout << "Someone wants a spoon!" << endl;
-                        MPI_Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &status); //receives the request for a spoon
+                MPI_Iprobe(MPI_ANY_SOURCE, 1, MCW, &tag, MPI_STATUS_IGNORE); // check if a player wants to grab a spoon.
+								cout << "Checking for tag " << tag << endl;
+                if(tag){ // someone wants a spoon;
+                        cout << "Someone wants a spoon! " << data << endl;
+                        MPI_Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, 1, MCW, &status); //receives the request for a spoon
+												cout << "Recieved the request" << endl;
                         if(data > 0 && spoons > 0){ // if there are spoons left.
                                 MPI_Send(&data, 1, MPI_INT, 1, 1, MCW); // player is sent a spoon.
                                 int grabASpoon = -2;
@@ -182,7 +183,7 @@ int main(int argc, char  **argv){
                                 MPI_Send(&youLose, 1, MPI_INT, data, 1, MCW); // sends you lost message to player without a spoon.
                         }
                 }
-      }
+      		}
             
             //------------------//
             //------PLAYERS-----//
@@ -194,7 +195,7 @@ int main(int argc, char  **argv){
             // - players are to do nothing if not alive
             // - players are to check if another player has picked up a spoon
             
-            if(rank != 0){ // if not rank zero.
+            else if(rank != 0){ // if not rank zero.
                     if(isAlive){ // if a player is still in the game either playing or waiting for the next round to start.
                             if(!done){ // if a player does not yet have a spoon, but is still playing the game.
                                     MPI_Iprobe(0, MPI_ANY_TAG, MCW, &tag, MPI_STATUS_IGNORE); // check if the other players grabbed a spoon.
@@ -242,7 +243,8 @@ int main(int argc, char  **argv){
                                             cout << " I'm Winning! --- Rank:" << rank << " =" << newHand[0][0] << "-" << newHand[1][0] << "-" << newHand[2][0] << "-" << newHand[3][0] <<  endl;
                                                 int giveMeASpoon = 1;
                                                 MPI_Send(&giveMeASpoon, 1, MPI_INT, 0, 1, MCW); // ask rank 0 politely for a spoon.
-                                                MPI_Recv(&giveMeASpoon, 1, MPI_INT, 0, MPI_ANY_TAG, MCW, &status);
+																								cout << "Rank: " << rank << " asking for a spoon" << endl;
+                                                MPI_Recv(&giveMeASpoon, 1, MPI_INT, 0, 1, MCW, &status);
                                                 if(giveMeASpoon == -1) {
                                                     isAlive = false; // you lose!
                                                 }
